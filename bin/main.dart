4185c94d0fe88b84f856e46570717fffc9496087
mcclayac <anthony.mcclay@gmail.com>
2020-05-21 23:48:25 -0400

    Initial commit

---
+++ bin/main.dart
@@ -0,0 +1,15 @@
+//code 1.22
+main(List<String> arguments) {
+  String stringInterpolation = 'string ' + 'concatenation';
+  print(stringInterpolation);
+
+  // multi-line string
+  String multiLIneString = """
+      This is
+      a multi line
+      string.
+  """;
+  print(multiLIneString);
+  
+
+}
